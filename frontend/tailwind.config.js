f551393593d8358f3c8be37a7f6f783d948006d3
Tsvetislav <tsvetislavt99@gmail.com>
2022-11-24 12:08:54 +0200

    Add tailwind colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          orange: '#FC8019',
+          grey: '#808080',
+          white: '#F8F8F8',
+          black: '#202020'
+        },
+      }
+    },
   },
   plugins: [],
 }
